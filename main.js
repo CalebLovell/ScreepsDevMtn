195c73452b44c3144dc2e016b9b67ccfd686a2bb
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-27 19:43:18 -0600

    make room setup work

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const roomStartup = require('./roomStartup')
 
 module.exports.loop = () => {
-    // const RCL = Room.controller.level
-    console.log(JSON.stringify(Game.rooms))
-    console.log(Game.rooms.room.controller.level)
-    // if (RCL < 3) {
-        roomStartup
+    let currentRoom = Game.spawns.Spawn1.room.name // make currentRoom dynamically update using a for in loop (see deleting memory names below)
+    let RCL = Game.rooms[currentRoom].controller.level;
+
+    // Delete Dead Creep Memory Names
+    for (let name in Memory.creeps) {
+        if (Game.creeps[name] == undefined) {
+            delete Memory.creeps[name];
+            console.log(`${name} died. Memory cleared.`);
+        }
+    }
+
+    // Starting creep setup for new rooms under RCL 3
+    if (RCL < 3) {
+        roomStartup;
+    }
 }
-
-
-
-sdf
-{"W43N29":{"name":"W43N29","energyAvailable":21,"energyCapacityAvailable":300,"visual":{"roomName":"W43N29"}}}
--- roomStartup.js
+++ roomStartup.js
@@ -1,2 +1,53 @@
+// Importing roles
+const roleHarvester = require('./role.harvester')
+const roleUpgrader = require('./role.upgrader')
+const roleBuilder = require('./role.builder')
 
-Game.spawns.Spawn1.createCreep([WORK, CARRY, CARRY, MOVE, MOVE])
+// Name creeps based on Role + Game Time
+function namer(role) {
+    return `${role}${Game.time.toString()}`
+}
+
+// Setting number of max noob creeps
+let numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester'); // turn into reduce functions
+let numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
+
+// Controlling noob creeps
+for (let name in Game.creeps) {
+    let creep = Game.creeps[name];
+    if (creep.memory.role == 'harvester') {
+        roleHarvester.run(creep);
+    } else if (creep.memory.role == 'upgrader') {
+        roleUpgrader.run(creep);
+    } else if (creep.memory.role == 'builder') {
+        roleBuilder.run(creep);
+    }
+}
+
+// Game.spawn the noob creeps
+if (numberOfHarvesters < 8) {
+    Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], namer('harvester'),
+        {
+            memory: {
+                role: 'harvester',
+                HAVE_LOAD: false,
+            }
+        });
+} else if (numberOfUpgraders < 5) {
+    Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], namer('upgrader'),
+        {
+            memory: {
+                role: 'upgrader',
+                HAVE_LOAD: false,
+            }
+        });
+} else {
+    Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], namer('builder'),
+        {
+            memory: {
+                role: 'builder',
+                HAVE_LOAD: false,
+            }
+        });
+}
+
